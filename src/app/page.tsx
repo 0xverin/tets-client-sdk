348c8c5c8dabb40fe1945c53f1605b92e4cc9b02
0xverin <daqingchong0809@gmail.com>
2025-03-28 17:36:50 +0800

    test client-sdk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,45 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+'use client';
+import { ApiPromise, WsProvider } from "@polkadot/api";
+import { cryptoWaitReady } from "@polkadot/util-crypto";
+import { getChain } from "@heima-network/chaindata";
+import { createIdentityType,request } from "@heima-network/client-sdk";
+import { TypeRegistry } from "@polkadot/types";
+import { identity } from "@heima-network/parachain-api";
 
-export default function Home() {
+
+export default async function Home() {
+
+
+  const excute=async()=>{
+    await cryptoWaitReady();
+    const registry = new TypeRegistry();
+    registry.register(identity.types);
+    const api = new ApiPromise({
+        provider: new WsProvider(getChain("heima-dev").rpcs[0].url),
+        registry,
+    });
+    console.log(getChain("heima-dev").rpcs[0].url, "url");
+    await api.isReady;
+    const member = createIdentityType(api.registry, {
+        addressOrHandle: "0xverin@gmail.com",
+        type: "Email",
+    });
+    console.log(member.toHuman(), "member");      
+
+    const result = await request.requestEmailVerificationCode(
+        {
+            email: "0xverin@gmail.com",
+        },
+        // true,
+    );
+
+  }
+  
+
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <button onClick={excute}>Click me</button>
     </div>
   );
+
 }
